b44766b1134ee12237eb9a0cc46edebdf2682943
xlfengye <2476083228@qq.com>
2026-06-04 20:42:22 +0800

    Update 3405690.lua

---
+++ 3405690.lua
@@ -0,0 +1,28 @@
+-- ============================================================
+-- 官方网站: https://steamforge.hyper99.top/
+-- 联系 QQ: 2476083228
+-- 入库时间: 2026-06-04 16:48:39
+-- 游戏 AppID: 3405690
+-- 游戏类型: D加密游戏（保留 setManifestid）
+-- ============================================================
+
+addappid(3405690)
+addappid(3405691, 1, "12a93ecb44c6b853d762578ffef21df60aa702f723232d87f81f485f7e636684")
+setManifestid(3405691, "3950846049273901666")
+addappid(3405692, 1, "c6ec306084ddbc34166f66738b51806ae01d351b007df9dbfb5a17531575b5b7")
+setManifestid(3405692, "8989839715920763746")
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491")
+setManifestid(3340991, "2898052249903094762")
+addappid(3405710)
+addappid(3405720)
+addappid(3484390)
+addappid(3484440)
+addappid(3484530)
+addappid(3707320)
+addappid(3765110)
+
+--[[
+This file Generated using fares.top Website 
+-----> join discord Server : https://discord.gg/steamgames 
+]]
