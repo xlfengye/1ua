8961d520af08fb176b236fd2c6d61ee15b19b875
2476083228 <1157079671@qq.com>
2026-06-22 20:23:49 +0800

    Update 2304670.lua

--- 2304670.lua
+++ 2304670.lua
@@ -1,4 +1,2 @@
 addappid(2304670)
-addappid(2304671,0,"f004f1e13bf2a7b0a606c60d1828f0ffe3a5b9ffdd2db790ae3fbf41fd2194bb")
-setManifestid(2304671,"524873800770036119")
-addappid(2304672)
+addappid(2304671, 0, "f004f1e13bf2a7b0a606c60d1828f0ffe3a5b9ffdd2db790ae3fbf41fd2194bb")
