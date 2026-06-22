517bff8a2696a6099b6cdf13a8677e83e7d13c19
2476083228 <1157079671@qq.com>
2026-06-22 22:08:50 +0800

    Update 401350.lua

--- 401350.lua
+++ 401350.lua
@@ -1,4 +1,2 @@
 addappid(401350)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(401351,0,"9ff94bbbc5bb2df2734bd0c6092b41f5e7d310ed7e5b445c9f3ec97c4df1a4dd")
+addappid(401351, 0, "9ff94bbbc5bb2df2734bd0c6092b41f5e7d310ed7e5b445c9f3ec97c4df1a4dd")
