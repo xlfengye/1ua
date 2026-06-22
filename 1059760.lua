f15e87f2ff345a4707329a87d093d0f34cc39bd6
2476083228 <1157079671@qq.com>
2026-06-22 20:22:25 +0800

    Update 1059760.lua

--- 1059760.lua
+++ 1059760.lua
@@ -1,4 +1,2 @@
 addappid(1059760)
-addappid(1059761,0,"c4c2df1557af69d65b85a3be1fe8f0691ab0de03e803e35defbef4cc03c8eacf")
-setManifestid(1059761,"3002774863070334337")
-addappid(1059762)
+addappid(1059761, 0, "c4c2df1557af69d65b85a3be1fe8f0691ab0de03e803e35defbef4cc03c8eacf")
