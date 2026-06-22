69588ef89fc30ca800c7685f495128725c0de92e
2476083228 <1157079671@qq.com>
2026-06-23 00:33:19 +0800

    Update 3144650.lua

--- 3144650.lua
+++ 3144650.lua
@@ -1,6 +1,2 @@
 addappid(3144650)
-addappid(228989)
-setManifestid(228989,"550968249685141759")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
 addappid(3144651,0,"fdceff5775b98adbc9c2bafc8eca4af7e3d5a83a594b628936dc12f811dfb5ae")
