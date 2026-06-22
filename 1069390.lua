5c92f542d1473623849aa32e39f501aac6277d04
xlfengye <2476083228@qq.com>
2026-06-22 14:41:14 +0800

    Update 1069390.lua

--- 1069390.lua
+++ 1069390.lua
@@ -1,5 +1,2 @@
 addappid(1069390)
-addappid(1069391,0,"2057cefeeaea92b7c4cd09e30756a437478d2a6ed9ba1a2ff4b76ce892abd8fa")
-setManifestid(1069391,"7084645421541473253")
-addappid(1154280)
-addappid(1160990)
+addappid(1069391, 0, "2057cefeeaea92b7c4cd09e30756a437478d2a6ed9ba1a2ff4b76ce892abd8fa")
