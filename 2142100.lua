d87ef83cd28b2648d2e44465b95738378c6790fb
2476083228 <1157079671@qq.com>
2026-06-22 20:23:42 +0800

    Update 2142100.lua

--- 2142100.lua
+++ 2142100.lua
@@ -1,5 +1,2 @@
 addappid(2142100)
-addappid(2142101,0,"bcc3fe18a4cc6a8c52cdbec90ae05a40b7edc3db30ac34865e5d60873aabf6cb")
-setManifestid(2142101,"2880557158607208175")
-addappid(2142103)
-addappid(2142102)
+addappid(2142101, 0, "bcc3fe18a4cc6a8c52cdbec90ae05a40b7edc3db30ac34865e5d60873aabf6cb")
