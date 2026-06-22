8c233e92dcd48898f407d85730415a2e352a9eac
2476083228 <1157079671@qq.com>
2026-06-22 22:15:47 +0800

    Update 845290.lua

--- 845290.lua
+++ 845290.lua
@@ -1,4 +1,2 @@
 addappid(845290)
-addappid(845299)
-addappid(845291,0,"95c24eff2ba823efd62f0c0aeaf2182e0adf872c1aa6b4e3ab5044372cabdb2d")
-setManifestid(845291,"8950747530175103882")
+addappid(845291, 0, "95c24eff2ba823efd62f0c0aeaf2182e0adf872c1aa6b4e3ab5044372cabdb2d")
