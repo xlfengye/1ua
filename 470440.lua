a11f382b48cc1fc98939038b49ac818aa8c0a211
2476083228 <1157079671@qq.com>
2026-06-22 22:14:37 +0800

    Update 470440.lua

--- 470440.lua
+++ 470440.lua
@@ -1,5 +1,2 @@
 addappid(470440)
-addappid(470441,0,"1bd95eeac1287a7af121597c1257a66eb0b8d7a4d0928fae7fab08a6ceb5eddf")
-setManifestid(470441,"3628023193262187339")
-addappid(470442)
-addappid(470443)
+addappid(470441, 0, "1bd95eeac1287a7af121597c1257a66eb0b8d7a4d0928fae7fab08a6ceb5eddf")
