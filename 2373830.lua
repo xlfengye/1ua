8093e2a0aa3944cfb0e432ec966af7c36358a9e9
xlfengye <2476083228@qq.com>
2026-06-22 15:34:06 +0800

    Update 2373830.lua

--- 2373830.lua
+++ 2373830.lua
@@ -1,5 +1,2 @@
 addappid(2373830)
-addappid(2373831,0,"b4299c05dad3377ae0b03edefcc543fdf68c6f3f9c8e7b9ed0c18b4ae728bd11")
-setManifestid(2373831,"6282839221661357416")
-addappid(2373832)
-addappid(2373833)
+addappid(2373831, 0, "b4299c05dad3377ae0b03edefcc543fdf68c6f3f9c8e7b9ed0c18b4ae728bd11")
