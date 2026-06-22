674e9842b086e421b57a49c59e8a4a766b62fdb9
xlfengye <2476083228@qq.com>
2026-06-22 14:41:30 +0800

    Update 1117330.lua

--- 1117330.lua
+++ 1117330.lua
@@ -1,4 +1,2 @@
 addappid(1117330)
-addappid(1117331,0,"8a2d8d2a2c92cdc24237cd9d92065262c30cfd9db1adaab35d0efc9afbf2eb70")
-setManifestid(1117331,"7113086000602398233")
-addappid(1187750)
+addappid(1117331, 0, "8a2d8d2a2c92cdc24237cd9d92065262c30cfd9db1adaab35d0efc9afbf2eb70")
