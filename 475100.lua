475260a6ee69c393b9deeec39d2d6ee6528c3080
2476083228 <1157079671@qq.com>
2026-06-22 20:26:43 +0800

    Update 475100.lua

--- 475100.lua
+++ 475100.lua
@@ -1,4 +1,2 @@
 addappid(475100)
-addappid(475101,0,"3e9fe8a1aea91be872c5ba57b2ce63a9fac437db18bcdfc0aebf3e38251f6a26")
-setManifestid(475101,"6020449906457278706")
-addappid(475102)
+addappid(475101, 0, "3e9fe8a1aea91be872c5ba57b2ce63a9fac437db18bcdfc0aebf3e38251f6a26")
