258f74bf3a290549ba379d2a69db0084b519e645
2476083228 <1157079671@qq.com>
2026-06-23 00:31:50 +0800

    Update 2818100.lua

--- 2818100.lua
+++ 2818100.lua
@@ -1,6 +1,2 @@
 addappid(2818100)
-addappid(228989)
-setManifestid(228989,"550968249685141759")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
 addappid(2818101,0,"9ef0fc2d1bfc7cb6d3b6e5fcdf8dda6db5493a74417864ca5bb07b39f25b9b6f")
