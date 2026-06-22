177008e1702e3b204898a2bca6e48aa40579cbda
xlfengye <2476083228@qq.com>
2026-06-22 22:27:37 +0800

    Update 609850.lua

--- 609850.lua
+++ 609850.lua
@@ -1,4 +1,2 @@
 addappid(609850)
-addappid(609851,0,"2b295bbe6f188c1ab07b7eff0b5f88efff9e172caba5661b28ba254e9fb099ee")
-setManifestid(609851,"4633351093287211600")
-addappid(609852)
+addappid(609851, 0, "2b295bbe6f188c1ab07b7eff0b5f88efff9e172caba5661b28ba254e9fb099ee")
