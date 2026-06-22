178853c1e600cad6f0423879168c5db3aa3b1ceb
2476083228 <1157079671@qq.com>
2026-06-22 16:02:19 +0800

    Update 620340.lua

--- 620340.lua
+++ 620340.lua
@@ -1,5 +1,2 @@
 addappid(620340)
-addappid(620341,0,"d3d03c9187927703affaeff633325dc38fc87b6fa0e9be8edab5e54455cfe0b1")
-setManifestid(620341,"8891966582858815674")
-addappid(620342)
-addappid(620343)
+addappid(620341, 0, "d3d03c9187927703affaeff633325dc38fc87b6fa0e9be8edab5e54455cfe0b1")
