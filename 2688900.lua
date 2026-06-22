5171c9b095a703b324e461def09655763644c419
2476083228 <1157079671@qq.com>
2026-06-23 00:31:22 +0800

    Update 2688900.lua

--- 2688900.lua
+++ 2688900.lua
@@ -1,5 +1,2 @@
 addappid(2688900)
 addappid(2688901,0,"af3bdc55ceadb64dab94346bdf46c89df1fef2af3ba5813a262b4299d418c6de")
-setManifestid(2688901,"1175910118687973227")
-addappid(2688902)
-addappid(2688903)
