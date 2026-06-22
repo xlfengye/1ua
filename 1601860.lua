67eb86005f68d0dac942135eb76612fa1f9741dd
xlfengye <2476083228@qq.com>
2026-06-22 14:43:33 +0800

    Update 1601860.lua

--- 1601860.lua
+++ 1601860.lua
@@ -1,5 +1,2 @@
 addappid(1601860)
-addappid(1601861,0,"9ec9b961fa3ac2616d4aa5042b89ffa25bb6ec49f5a7fe6a6abdd64a69ecd8e0")
-setManifestid(1601861,"1940016071938415041")
-addappid(1601862)
-addappid(1601863)
+addappid(1601861, 0, "9ec9b961fa3ac2616d4aa5042b89ffa25bb6ec49f5a7fe6a6abdd64a69ecd8e0")
