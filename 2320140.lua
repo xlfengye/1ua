cb096fbfec292c2c9e557f35c9e7f3247fbe678c
2476083228 <1157079671@qq.com>
2026-06-22 23:05:49 +0800

    Update 2320140.lua

--- 2320140.lua
+++ 2320140.lua
@@ -1,7 +1,2 @@
 addappid(2320140)
-addappid(228989)
-setManifestid(228989,"550968249685141759")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(2320141,0,"798e0b6c2d3a3ee1dba7bc7a8cad9a4834209b1fd5d1def8c54183b8cda5a3cc")
-addappid(1491005)
+addappid(2320141, 0, "798e0b6c2d3a3ee1dba7bc7a8cad9a4834209b1fd5d1def8c54183b8cda5a3cc")
