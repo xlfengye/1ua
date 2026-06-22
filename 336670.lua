4565bbfb0e7df72d084de56eeea77a76bfa53a58
xlfengye <2476083228@qq.com>
2026-06-22 22:29:46 +0800

    Update 336670.lua

--- 336670.lua
+++ 336670.lua
@@ -1,5 +1,2 @@
 addappid(336670)
-addappid(228985)
-setManifestid(228985,"3966345552745568756")
-addappid(336671,0,"8d9c30dba5ce01dca48dd7c896d161260f028833abb42fba96009dcbc667fbbf")
-addappid(346870)
+addappid(336671, 0, "8d9c30dba5ce01dca48dd7c896d161260f028833abb42fba96009dcbc667fbbf")
