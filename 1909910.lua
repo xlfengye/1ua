9b8b6bbe5b4cff286c67d2bcc8dc61cf590f040c
xlfengye <2476083228@qq.com>
2026-06-22 22:28:40 +0800

    Update 1909910.lua

--- 1909910.lua
+++ 1909910.lua
@@ -1,6 +1,2 @@
 addappid(1909910)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1909911,0,"578b1157ae4a99eccdad9a81fb1e8bbfaf72bd07965d0d3d2202ddb6a7ecf8b7")
+addappid(1909911, 0, "578b1157ae4a99eccdad9a81fb1e8bbfaf72bd07965d0d3d2202ddb6a7ecf8b7")
