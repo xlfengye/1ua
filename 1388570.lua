293ab1d999b5e85397b02c90a8048afdcc489562
xlfengye <2476083228@qq.com>
2026-06-22 20:40:52 +0800

    Update 1388570.lua

--- 1388570.lua
+++ 1388570.lua
@@ -1,4 +1,2 @@
 addappid(1388570)
-addappid(1388571,0,"d9c8da7ce9eaf53f6dab84ce7769ffdb38d45b0dc48976de4a4b38547effea03")
-setManifestid(1388571,"8595865598225253845")
-addappid(1388572)
+addappid(1388571, 0, "d9c8da7ce9eaf53f6dab84ce7769ffdb38d45b0dc48976de4a4b38547effea03")
