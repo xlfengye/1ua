91d85bcfb5b49836155c2a5c0b1baa6e770e148f
2476083228 <1157079671@qq.com>
2026-06-22 22:10:49 +0800

    Update 955910.lua

--- 955910.lua
+++ 955910.lua
@@ -1,4 +1,2 @@
 addappid(955910)
-addappid(228987)
-setManifestid(228987,"4302102680580581867")
-addappid(955911,0,"a3ee17d3fbf678e2b1920d00639b1fb6b8da82abcdfde1edf9a3c1b55de0f09f")
+addappid(955911, 0, "a3ee17d3fbf678e2b1920d00639b1fb6b8da82abcdfde1edf9a3c1b55de0f09f")
