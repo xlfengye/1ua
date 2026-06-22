a2b8c0dc5f856cb7222d7bdbb35b58dabb917d18
2476083228 <1157079671@qq.com>
2026-06-22 16:04:18 +0800

    Update 990030.lua

--- 990030.lua
+++ 990030.lua
@@ -1,6 +1,2 @@
 addappid(990030)
-addappid(990031,0,"ce230fc27d73d88670cfed51b26c7df47e69f8d1a6cf67dfc20519ada7b3ceda")
-setManifestid(990031,"6063428795463017622")
-addappid(1096081)
-addappid(1096080)
-addappid(1115600)
+addappid(990031, 0, "ce230fc27d73d88670cfed51b26c7df47e69f8d1a6cf67dfc20519ada7b3ceda")
