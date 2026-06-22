5a3cf9a434cf7ef5c9c33198fa9f0a1dc53dd7f5
2476083228 <1157079671@qq.com>
2026-06-22 23:07:17 +0800

    Update 63920.lua

--- 63920.lua
+++ 63920.lua
@@ -1,5 +1,2 @@
 addappid(63920)
-addappid(63912,0,"92c67afcdca1ce026d46e83cb5f8d1cc52b1c16bba24d0b1f3073aa67aa7f1e3")
-setManifestid(63912,"3712792488926387762")
-addappid(63911)
-setManifestid(63911,"5364857015418105491")
+addappid(63912, 0, "92c67afcdca1ce026d46e83cb5f8d1cc52b1c16bba24d0b1f3073aa67aa7f1e3")
