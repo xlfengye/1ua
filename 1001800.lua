f2935088ce8d19c593dc253f872c4bd915338b43
xlfengye <2476083228@qq.com>
2026-06-22 14:40:51 +0800

    Update 1001800.lua

--- 1001800.lua
+++ 1001800.lua
@@ -1,5 +1,2 @@
 addappid(1001800)
-addappid(1001801,0,"9fd487a3da0d1aab519aa3388503ce5baab0f4b767b0fd8ddafb55db5c815b37")
-setManifestid(1001801,"5799662960526131947")
-addappid(1001802)
-addappid(1001803)
+addappid(1001801, 0, "9fd487a3da0d1aab519aa3388503ce5baab0f4b767b0fd8ddafb55db5c815b37")
