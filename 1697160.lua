d76c8c21e9e715b9d2a79aaf2b38f0abf1a52400
xlfengye <2476083228@qq.com>
2026-06-22 20:43:14 +0800

    Update 1697160.lua

--- 1697160.lua
+++ 1697160.lua
@@ -1,6 +1,2 @@
 addappid(1697160)
-addappid(228989)
-setManifestid(228989,"550968249685141759")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1697161,0,"9aba9cb48307cd43025f7ac7f712d9ae6bebb8f254e32ec5bbd0ea44dbf897cc")
+addappid(1697161, 0, "9aba9cb48307cd43025f7ac7f712d9ae6bebb8f254e32ec5bbd0ea44dbf897cc")
