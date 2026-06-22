4889ffefe9deaac5bab720191a39352bd4704b24
xlfengye <2476083228@qq.com>
2026-06-22 22:38:28 +0800

    Update 2431330.lua

--- 2431330.lua
+++ 2431330.lua
@@ -1,4 +1,2 @@
 addappid(2431330)
-addappid(228989)
-setManifestid(228989,"550968249685141759")
-addappid(2431331,0,"a0edb42b05deea4ad03083ded3c83d4f6e310ddfec668f48746c4eae6c70dc9e")
+addappid(2431331, 0, "a0edb42b05deea4ad03083ded3c83d4f6e310ddfec668f48746c4eae6c70dc9e")
