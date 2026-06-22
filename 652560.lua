044825485852ba3bca8fb039ee83c1d82c118ddb
2476083228 <1157079671@qq.com>
2026-06-22 22:09:58 +0800

    Update 652560.lua

--- 652560.lua
+++ 652560.lua
@@ -1,4 +1,2 @@
 addappid(652560)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(652561,0,"fa2d2e70ffdc5deebb0e7001698edac402643fe94c5f4b3cf8ad9d3e253eab94")
+addappid(652561, 0, "fa2d2e70ffdc5deebb0e7001698edac402643fe94c5f4b3cf8ad9d3e253eab94")
