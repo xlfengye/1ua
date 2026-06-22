f74b02f46756b256abd770ab9afecef774db55b9
2476083228 <1157079671@qq.com>
2026-06-22 16:02:39 +0800

    Update 670610.lua

--- 670610.lua
+++ 670610.lua
@@ -1,4 +1,2 @@
 addappid(670610)
-addappid(670611,0,"a9dbbb20e67ccc00b07a352d3c081a2c20fe4c61a7fd56a4a7d8c2dce59544fe")
-setManifestid(670611,"3717249402734069157")
-addappid(670612)
+addappid(670611, 0, "a9dbbb20e67ccc00b07a352d3c081a2c20fe4c61a7fd56a4a7d8c2dce59544fe")
