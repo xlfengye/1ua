5140d3f2ee77ac2d761ce088fbb048d05fbb4e10
xlfengye <2476083228@qq.com>
2026-06-22 15:32:58 +0800

    Update 1979320.lua

--- 1979320.lua
+++ 1979320.lua
@@ -1,4 +1,2 @@
 addappid(1979320)
-addappid(1979321,0,"acbb7f2b7917baba597c6abf590b8aa6ed5eb42de29142eac08aab17a52d01fa")
-setManifestid(1979321,"3432788794017755953")
-addappid(1979322)
+addappid(1979321, 0, "acbb7f2b7917baba597c6abf590b8aa6ed5eb42de29142eac08aab17a52d01fa")
