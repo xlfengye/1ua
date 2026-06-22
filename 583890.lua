c03895b3f865a475efe23c69f543290b9f707198
2476083228 <1157079671@qq.com>
2026-06-22 16:02:04 +0800

    Update 583890.lua

--- 583890.lua
+++ 583890.lua
@@ -1,6 +1,2 @@
 addappid(583890)
-addappid(583891,0,"1ef492f0e647d9e4a372eb1fc2e6ff5107f8fe0f1e1fd7ccdec88ac59eb3f5c2")
-setManifestid(583891,"4420902273324993248")
-addappid(583892)
-addappid(647920)
-addappid(583893)
+addappid(583891, 0, "1ef492f0e647d9e4a372eb1fc2e6ff5107f8fe0f1e1fd7ccdec88ac59eb3f5c2")
