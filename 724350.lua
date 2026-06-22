7035aa748e79f62fb7692704856e2067079449a1
xlfengye <2476083228@qq.com>
2026-06-22 22:33:26 +0800

    Update 724350.lua

--- 724350.lua
+++ 724350.lua
@@ -1,4 +1,2 @@
 addappid(724350)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(724351,0,"89be9e8650dbeeac904a37ccfd40b40e8518f0cdabdfbd2ea396702c9a958c67")
+addappid(724351, 0, "89be9e8650dbeeac904a37ccfd40b40e8518f0cdabdfbd2ea396702c9a958c67")
