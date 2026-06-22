0781e674ce1e23cea18d59e57d086c7079c70f42
2476083228 <1157079671@qq.com>
2026-06-22 16:00:45 +0800

    Update 403560.lua

--- 403560.lua
+++ 403560.lua
@@ -1,4 +1,2 @@
 addappid(403560)
-addappid(403561,0,"d3d711fbb04ae6fa67a2e1db8bab4dd2af7f0f6399c8dfb6f51caad7eef54c55")
-setManifestid(403561,"2426991217674960215")
-addappid(406360)
+addappid(403561, 0, "d3d711fbb04ae6fa67a2e1db8bab4dd2af7f0f6399c8dfb6f51caad7eef54c55")
