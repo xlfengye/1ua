12b3390e910f63356fe0aed58a2d476d4235575b
2476083228 <1157079671@qq.com>
2026-06-23 00:35:07 +0800

    Update 945880.lua

--- 945880.lua
+++ 945880.lua
@@ -1,6 +1,2 @@
 addappid(945880)
-addappid(228986)
-setManifestid(228986,"8782296191957114623")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
 addappid(945881,0,"7a0e881177bded0da7d61ad82ad04ff24e856ba8abaaec084ffcbcfc7f91112e")
