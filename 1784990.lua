2ccb95fbd74c695efee691df76e88cd3cd06c157
xlfengye <2476083228@qq.com>
2026-06-22 15:32:33 +0800

    Update 1784990.lua

--- 1784990.lua
+++ 1784990.lua
@@ -1,5 +1,2 @@
 addappid(1784990)
-addappid(1784991,0,"d4ca89593ebbb9bdbc0d6cc6ffc13e18c604ccd795eaa6b3c1e6b371a7cb8014")
-setManifestid(1784991,"4138985018957620299")
-addappid(1784992)
-addappid(1784993)
+addappid(1784991, 0, "d4ca89593ebbb9bdbc0d6cc6ffc13e18c604ccd795eaa6b3c1e6b371a7cb8014")
