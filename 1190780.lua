9bab74a3786b2594e64d4435c4f678ad4c093533
xlfengye <2476083228@qq.com>
2026-06-22 14:24:04 +0800

    Update 1190780.lua

--- 1190780.lua
+++ 1190780.lua
@@ -1,7 +1,2 @@
 addappid(1190780)
-addappid(228986)
-setManifestid(228986,"8782296191957114623")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1190781,0,"fd3f2ccda0c5eea9c0296fdd4dcbebecc3fc07de6444bd2f3cd278b0d97d0932")
-setManifestid(1190781,"7160068342213166427")
+addappid(1190781, 0, "fd3f2ccda0c5eea9c0296fdd4dcbebecc3fc07de6444bd2f3cd278b0d97d0932")
