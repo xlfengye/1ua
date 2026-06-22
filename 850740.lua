32563375039488bebc5aefc9aa3ad8eb032b664b
2476083228 <1157079671@qq.com>
2026-06-22 16:03:35 +0800

    Update 850740.lua

--- 850740.lua
+++ 850740.lua
@@ -1,5 +1,2 @@
 addappid(850740)
-addappid(850741,0,"3931b61ea6a3bb2cdf44df79a2fd4cc3d1e054ae6533ccb3f48bef07bc559db7")
-setManifestid(850741,"1871119191075705726")
-addappid(850742)
-addappid(850743)
+addappid(850741, 0, "3931b61ea6a3bb2cdf44df79a2fd4cc3d1e054ae6533ccb3f48bef07bc559db7")
