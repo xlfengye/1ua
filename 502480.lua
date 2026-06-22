d17303a1626d74d01c7788e24a23254af8cc08c2
xlfengye <2476083228@qq.com>
2026-06-22 22:27:22 +0800

    Update 502480.lua

--- 502480.lua
+++ 502480.lua
@@ -1,4 +1,2 @@
 addappid(502480)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(502482,0,"fb4267efb3a106d6eddc7f46c550e6014727f04ad4d1ace7aa0e2cccf2697c0e")
+addappid(502482, 0, "fb4267efb3a106d6eddc7f46c550e6014727f04ad4d1ace7aa0e2cccf2697c0e")
