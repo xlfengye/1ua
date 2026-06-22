92f385aaaf0d2ee0ca8a4e2e2c91514ba5787f14
2476083228 <1157079671@qq.com>
2026-06-22 23:05:44 +0800

    Update 2196700.lua

--- 2196700.lua
+++ 2196700.lua
@@ -1,6 +1,2 @@
 addappid(2196700)
-addappid(228989)
-setManifestid(228989,"550968249685141759")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(2196701,0,"dffecca66b9bc4efba890e2c8dbf4cef9e483686069805cda34f6fc80c254c8e")
+addappid(2196701, 0, "dffecca66b9bc4efba890e2c8dbf4cef9e483686069805cda34f6fc80c254c8e")
