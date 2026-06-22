8339510818866b7c5eb5d2658b08e4b5cab0f602
xlfengye <2476083228@qq.com>
2026-06-22 14:41:03 +0800

    Update 1038620.lua

--- 1038620.lua
+++ 1038620.lua
@@ -1,5 +1,3 @@
 addappid(1038620)
-addappid(1038621,0,"4588b7efacca1252df5ccc4cedf1bc1dec480ebe5a9fe5a79ab1fc7ca51cbc53")
-setManifestid(1038621,"5169762982327172988")
-addappid(1038622)
-addappid(1038623)
+addappid(1038621, 0, "4588b7efacca1252df5ccc4cedf1bc1dec480ebe5a9fe5a79ab1fc7ca51cbc53")
+addappid(1038623, 0, "28db1adb1fd9ddbd186a47ad9d5e95acb839e493ce748cc1a08604c10066d581")
