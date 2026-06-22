53aae6ff3449367a760e7c7b6c8cb046393eb6e4
xlfengye <2476083228@qq.com>
2026-06-22 22:31:52 +0800

    Update 1863820.lua

--- 1863820.lua
+++ 1863820.lua
@@ -1,6 +1,2 @@
 addappid(1863820)
-addappid(228989)
-setManifestid(228989,"550968249685141759")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1863821,0,"3dce3f2ae9dfedb5b584ecff61dfec408daa858f58fbbf1369cc14478916a39d")
+addappid(1863821, 0, "3dce3f2ae9dfedb5b584ecff61dfec408daa858f58fbbf1369cc14478916a39d")
