1f7af7dd8908f7df1507cab4d30c22f85a9c193f
2476083228 <1157079671@qq.com>
2026-06-23 00:33:41 +0800

    Update 9040.lua

--- 9040.lua
+++ 9040.lua
@@ -1,5 +1,2 @@
 addappid(9040)
-addappid(2311)
-setManifestid(2311,"2932104123765884370")
 addappid(9041,0,"a6a446da6a42feddcac660cd0416e5128f91caa601bf5bfb747e45980063cbf1")
-setManifestid(9041,"7374147715847291499")
