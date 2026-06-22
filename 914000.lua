51bb7bea881b97bf819e06e3529ba88a061b5216
2476083228 <1157079671@qq.com>
2026-06-22 16:03:57 +0800

    Update 914000.lua

--- 914000.lua
+++ 914000.lua
@@ -1,4 +1,2 @@
 addappid(914000)
-addappid(914001,0,"04477505f5ef161fe4bf9ed30d89455eba4bfcaeaedaf679e6c987de36a4a0a9")
-setManifestid(914001,"3115937955078708786")
-addappid(914002)
+addappid(914001, 0, "04477505f5ef161fe4bf9ed30d89455eba4bfcaeaedaf679e6c987de36a4a0a9")
