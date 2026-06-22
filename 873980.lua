042a9ca77909b0587e5446641b327b7fcbf1e846
2476083228 <1157079671@qq.com>
2026-06-22 16:03:46 +0800

    Update 873980.lua

--- 873980.lua
+++ 873980.lua
@@ -1,6 +1,2 @@
 addappid(873980)
-addappid(873981,0,"de5d916cf73ac8340e1ccbcefd5c2e118bcfa9f4e479c085f102fdf8e61648af")
-setManifestid(873981,"1376024171075355650")
-addappid(873982)
-addappid(873983)
-addappid(873984)
+addappid(873981, 0, "de5d916cf73ac8340e1ccbcefd5c2e118bcfa9f4e479c085f102fdf8e61648af")
