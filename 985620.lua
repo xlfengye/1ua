8a57faa9a9d8b3bd17dad3a6b01a352c7efa9a6c
2476083228 <1157079671@qq.com>
2026-06-22 16:04:17 +0800

    Update 985620.lua

--- 985620.lua
+++ 985620.lua
@@ -1,4 +1,2 @@
 addappid(985620)
-addappid(985621,0,"6a7f5dc9b06a35cf0a189f2b45e23e26d2e7bcfd6c92dbcb93523fdb6db5f9d0")
-setManifestid(985621,"2496255874893789536")
-addappid(985622)
+addappid(985621, 0, "6a7f5dc9b06a35cf0a189f2b45e23e26d2e7bcfd6c92dbcb93523fdb6db5f9d0")
