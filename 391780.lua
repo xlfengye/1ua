03b09c79008b5d0cb790f7f41bdbbd2c4c2ae86f
xlfengye <2476083228@qq.com>
2026-06-22 22:27:06 +0800

    Update 391780.lua

--- 391780.lua
+++ 391780.lua
@@ -1,4 +1,2 @@
 addappid(391780)
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(391781,0,"55ffa28b8ed21dbaa7e54eb7ce6ac7c3264aab9bbec2b418d59315f51682e7d8")
+addappid(391781, 0, "55ffa28b8ed21dbaa7e54eb7ce6ac7c3264aab9bbec2b418d59315f51682e7d8")
