63fa6f199f6e8e168b47250cc57c03f34d5d613a
2476083228 <1157079671@qq.com>
2026-06-22 22:10:52 +0800

    Update 975270.lua

--- 975270.lua
+++ 975270.lua
@@ -1,4 +1,2 @@
 addappid(975270)
-addappid(975271,0,"bfceb441fc0553710d51a37ca17f0abf31cebfd67fd874fdfee28bd5f913c40a")
-setManifestid(975271,"4975412833242773862")
-addappid(1681201)
+addappid(975271, 0, "bfceb441fc0553710d51a37ca17f0abf31cebfd67fd874fdfee28bd5f913c40a")
