22108d20b25f007c41ef03398c6dcace4893ac98
xlfengye <2476083228@qq.com>
2026-06-22 14:42:58 +0800

    Update 1423780.lua

--- 1423780.lua
+++ 1423780.lua
@@ -1,6 +1,3 @@
 addappid(1423780)
-addappid(1423785,0,"32bd6157a46bdda2da5a21f25ebf8ea315d5d63b5638eff3ad5d4dbd02cc25d0")
-setManifestid(1423785,"2871608615038357429")
-addappid(1423781)
-addappid(1423782)
-addappid(1423783)
+addappid(1423783, 0, "6c28ef9bef1d815b3911bca448b8ab762650fbae1eb504ee0ae4af1a361bf4bc")
+addappid(1423785, 0, "32bd6157a46bdda2da5a21f25ebf8ea315d5d63b5638eff3ad5d4dbd02cc25d0")
