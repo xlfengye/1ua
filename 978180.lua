f3be41291f926133c3e7de558d7183272351f96d
2476083228 <1157079671@qq.com>
2026-06-22 20:25:50 +0800

    Update 978180.lua

--- 978180.lua
+++ 978180.lua
@@ -1,4 +1,2 @@
 addappid(978180)
-addappid(978181,0,"01e52ddabcf80a35fbf40a658109b3dfa5b1e32204ca1fc1c00c26d239d5ddce")
-setManifestid(978181,"1670867343041555940")
-addappid(978182)
+addappid(978181, 0, "01e52ddabcf80a35fbf40a658109b3dfa5b1e32204ca1fc1c00c26d239d5ddce")
