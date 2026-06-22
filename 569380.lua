50d9be25ae677b8dd70e4f27ea04ee4edbe40043
2476083228 <1157079671@qq.com>
2026-06-22 16:01:58 +0800

    Update 569380.lua

--- 569380.lua
+++ 569380.lua
@@ -1,4 +1,2 @@
 addappid(569380)
-addappid(569381,0,"0a903cfda7735978ee8a9cd61faf26119d79cbf383a1ceaeec26d39b19a10bfa")
-setManifestid(569381,"4492879705101975313")
-addappid(569382)
+addappid(569381, 0, "0a903cfda7735978ee8a9cd61faf26119d79cbf383a1ceaeec26d39b19a10bfa")
