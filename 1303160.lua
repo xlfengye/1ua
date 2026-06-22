f36923059c52453c6fd854e148408d5655538891
xlfengye <2476083228@qq.com>
2026-06-22 14:36:03 +0800

    Update 1303160.lua

--- 1303160.lua
+++ 1303160.lua
@@ -1,4 +1,2 @@
 addappid(1303160)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(1303161,0,"95c8ef2a272b20bfbdca860e98b6bc2ac4dcb2adb3d5882507ef95bc57e689ab")
+addappid(1303161, 0, "95c8ef2a272b20bfbdca860e98b6bc2ac4dcb2adb3d5882507ef95bc57e689ab")
