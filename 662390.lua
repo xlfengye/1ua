99171cfef270a7df8df21e5caf376fe0b53ea75b
2476083228 <1157079671@qq.com>
2026-06-22 16:02:36 +0800

    Update 662390.lua

--- 662390.lua
+++ 662390.lua
@@ -1,4 +1,2 @@
 addappid(662390)
-addappid(662391,0,"cd3c1898ccb4dde8f1c0a2126079d7c344aaef4963e70fdb70fc3af635d8ac0e")
-setManifestid(662391,"258300164146104051")
-addappid(662392)
+addappid(662391, 0, "cd3c1898ccb4dde8f1c0a2126079d7c344aaef4963e70fdb70fc3af635d8ac0e")
