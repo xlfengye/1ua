db52895e545c8ddb013a79d6a46908fbc26bd01d
2476083228 <1157079671@qq.com>
2026-06-22 16:02:09 +0800

    Update 597190.lua

--- 597190.lua
+++ 597190.lua
@@ -1,5 +1,2 @@
 addappid(597190)
-addappid(597191)
-addappid(597192,0,"dcb69a41ea627e6cdba0b2a96d1d27e9ea46e11060e9fc67335f5a738ea3ece4")
-setManifestid(597192,"323416402421056949")
-addappid(597193)
+addappid(597192, 0, "dcb69a41ea627e6cdba0b2a96d1d27e9ea46e11060e9fc67335f5a738ea3ece4")
