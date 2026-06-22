9aa8233ff642be691ccab10c91b10ca19c852ec3
2476083228 <1157079671@qq.com>
2026-06-22 16:02:14 +0800

    Update 611620.lua

--- 611620.lua
+++ 611620.lua
@@ -1,4 +1,2 @@
 addappid(611620)
-addappid(611621,0,"de844e1a387d3fb7b8ecc4a281c9c9f9dad7f1bcea9deaa21ee098f5e6f2967a")
-setManifestid(611621,"6822920661856152505")
-addappid(797460)
+addappid(611621, 0, "de844e1a387d3fb7b8ecc4a281c9c9f9dad7f1bcea9deaa21ee098f5e6f2967a")
