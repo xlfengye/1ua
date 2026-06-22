7e3a55fa3ca8c372d3cd0d04c9f2000f6837f96c
2476083228 <1157079671@qq.com>
2026-06-22 16:03:18 +0800

    Update 780260.lua

--- 780260.lua
+++ 780260.lua
@@ -1,5 +1,2 @@
 addappid(780260)
-addappid(780261,0,"e710547fdd1027fa2731b5af77b8af00dbce7a76c7f4b7fecee02011e8d9dba5")
-setManifestid(780261,"1677615645750374808")
-addappid(780262)
-addappid(780263)
+addappid(780261, 0, "e710547fdd1027fa2731b5af77b8af00dbce7a76c7f4b7fecee02011e8d9dba5")
