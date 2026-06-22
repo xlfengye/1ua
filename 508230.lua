c221074852d25e5582332674475da023f9a33d4c
2476083228 <1157079671@qq.com>
2026-06-22 16:01:32 +0800

    Update 508230.lua

--- 508230.lua
+++ 508230.lua
@@ -1,4 +1,2 @@
 addappid(508230)
-addappid(508231,0,"cf2ff8f42ea34bebb555f17f114ff4ce4fdc9595948dc9f941c8fb8aa07e4b53")
-setManifestid(508231,"2561594097377064764")
-addappid(548440)
+addappid(508231, 0, "cf2ff8f42ea34bebb555f17f114ff4ce4fdc9595948dc9f941c8fb8aa07e4b53")
