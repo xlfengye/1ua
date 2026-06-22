3d454e3a5abc1a3b508487fcda08386883eddeac
2476083228 <1157079671@qq.com>
2026-06-22 16:03:15 +0800

    Update 774201.lua

--- 774201.lua
+++ 774201.lua
@@ -1,4 +1,2 @@
 addappid(774201)
-addappid(774202,0,"bf4c8aec9ff3c9bf1f542f5bad624260cb3dd04cffba760393d97f0aacc78dd6")
-setManifestid(774202,"6790446026763573193")
-addappid(1066320)
+addappid(774202, 0, "bf4c8aec9ff3c9bf1f542f5bad624260cb3dd04cffba760393d97f0aacc78dd6")
