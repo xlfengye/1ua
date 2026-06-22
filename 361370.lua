c35e1a8cb839f63783f588e429628b1f95834316
2476083228 <1157079671@qq.com>
2026-06-22 16:00:29 +0800

    Update 361370.lua

--- 361370.lua
+++ 361370.lua
@@ -1,4 +1,2 @@
 addappid(361370)
-addappid(361371,0,"c1eee3ede16bebd082fc78224f086a3fcaf00bd326fdbef5500385f280b59c0d")
-setManifestid(361371,"30782876519845517")
-addappid(361372)
+addappid(361371, 0, "c1eee3ede16bebd082fc78224f086a3fcaf00bd326fdbef5500385f280b59c0d")
