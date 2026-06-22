1106f6708033cbb01a5c14ed5978bee24e393e38
2476083228 <1157079671@qq.com>
2026-06-22 22:15:35 +0800

    Update 769550.lua

--- 769550.lua
+++ 769550.lua
@@ -1,4 +1,3 @@
 addappid(769550)
-addappid(769551,0,"d29bcc6d01f8dd9af3d7a2d27f0eded7b685dc5071f7ff02ad7c97dbf05c5654")
-setManifestid(769551,"793647786892947889")
-addappid(769552)
+addappid(769551, 0, "d29bcc6d01f8dd9af3d7a2d27f0eded7b685dc5071f7ff02ad7c97dbf05c5654")
+addappid(769552, 0, "f71f4bd74b7feb0c7af9c7a0520f936f2e12826cfcfe6bd79d0bb7af6ae42faa")
