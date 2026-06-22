257cd72b9d4cf2e47b71acf781025d79ac5e5674
2476083228 <1157079671@qq.com>
2026-06-22 22:12:16 +0800

    Update 2001120.lua

--- 2001120.lua
+++ 2001120.lua
@@ -1,3 +1,2 @@
 addappid(2001120)
-addappid(3050960)
-addappid(2001121,0,"27f6aac2ce9d80ad4ed8fe3710d306ae108f00ddcf005d47ecf7bfa2a258964c")
+addappid(2001121, 0, "27f6aac2ce9d80ad4ed8fe3710d306ae108f00ddcf005d47ecf7bfa2a258964c")
