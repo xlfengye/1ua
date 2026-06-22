3267eae92b6925bc65b8b86f1c373bd9ba1658e4
2476083228 <1157079671@qq.com>
2026-06-22 22:06:32 +0800

    Update 1815770.lua

--- 1815770.lua
+++ 1815770.lua
@@ -1,6 +1,2 @@
 addappid(1815770)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1815771,0,"965e781ebb0c24ec38096da597046d2c2658ddc5bf7baeedd9e93cfcdfcde5f7")
+addappid(1815771, 0, "965e781ebb0c24ec38096da597046d2c2658ddc5bf7baeedd9e93cfcdfcde5f7")
