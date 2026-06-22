684cc848e040ad3f4a4ff2c445c6fc673424b2c0
2476083228 <1157079671@qq.com>
2026-06-22 22:11:50 +0800

    Update 1829800.lua

--- 1829800.lua
+++ 1829800.lua
@@ -1,6 +1,2 @@
 addappid(1829800)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1829801,0,"9e9a07c232214b6a9d1e7baeddc5f4cbd1a57daf95f79ffb9cd8bcca7e0b5809")
+addappid(1829801, 0, "9e9a07c232214b6a9d1e7baeddc5f4cbd1a57daf95f79ffb9cd8bcca7e0b5809")
