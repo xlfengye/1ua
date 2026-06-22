a120a61104f34940a1e4aebc79b0c8539b651dd5
2476083228 <1157079671@qq.com>
2026-06-22 15:59:47 +0800

    Update 324520.lua

--- 324520.lua
+++ 324520.lua
@@ -1,5 +1,2 @@
 addappid(324520)
-addappid(324521,0,"bc3fbec30a03a8f8e9fc0a991cc9279a0e4c4c2fc9e39e36bbf69440ea6ec0eb")
-setManifestid(324521,"9029723987251470180")
-addappid(324522)
-addappid(324523)
+addappid(324521, 0, "bc3fbec30a03a8f8e9fc0a991cc9279a0e4c4c2fc9e39e36bbf69440ea6ec0eb")
