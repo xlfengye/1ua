51966e0699e3f0f3e8b4852579efdfe2c9d848d6
xlfengye <2476083228@qq.com>
2026-06-22 22:28:02 +0800

    Update 870420.lua

--- 870420.lua
+++ 870420.lua
@@ -1,5 +1,2 @@
 addappid(870420)
-addappid(870421,0,"244f1ccebf6b74d0ee5ed37df994bff92d9d6ac6704f1143be04a3a8842eae42")
-setManifestid(870421,"3216868890476732291")
-addappid(870422)
-addappid(870423)
+addappid(870421, 0, "244f1ccebf6b74d0ee5ed37df994bff92d9d6ac6704f1143be04a3a8842eae42")
