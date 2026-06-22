f579546e31d2694253f77e18234fe866667b2381
xlfengye <2476083228@qq.com>
2026-06-22 22:30:24 +0800

    Update 613900.lua

--- 613900.lua
+++ 613900.lua
@@ -1,4 +1,2 @@
 addappid(613900)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(613901,0,"53cd0c4bfc97eab6b81e3ad8cc2b87f02befdcfee5593170ed6064fe770a8710")
+addappid(613901, 0, "53cd0c4bfc97eab6b81e3ad8cc2b87f02befdcfee5593170ed6064fe770a8710")
