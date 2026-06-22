aac41918658ad4a8367bda50caef7376ec259061
2476083228 <1157079671@qq.com>
2026-06-22 22:07:51 +0800

    Update 2556180.lua

--- 2556180.lua
+++ 2556180.lua
@@ -1,4 +1,2 @@
 addappid(2556180)
-addappid(228989)
-setManifestid(228989,"550968249685141759")
-addappid(2556181,0,"a2c170abf1d1fbd3b6da15b0b3ec9f50effcae0c3e2cf71ef0ef7d2612ab34fc")
+addappid(2556181, 0, "a2c170abf1d1fbd3b6da15b0b3ec9f50effcae0c3e2cf71ef0ef7d2612ab34fc")
