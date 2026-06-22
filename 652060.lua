a623be33a42badbcaa903205bb0eda927f69b078
2476083228 <1157079671@qq.com>
2026-06-22 23:12:09 +0800

    Update 652060.lua

--- 652060.lua
+++ 652060.lua
@@ -1,4 +1,2 @@
 addappid(652060)
-addappid(228985)
-setManifestid(228985,"3966345552745568756")
-addappid(652061,0,"bfd0f6fe5a1dbe368eef04d42ab92ee1b253e66cb19586035a3ee678db9aff98")
+addappid(652061, 0, "bfd0f6fe5a1dbe368eef04d42ab92ee1b253e66cb19586035a3ee678db9aff98")
