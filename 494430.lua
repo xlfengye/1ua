0a9ef1fd1526c3dfcab1b26fb3abeee0172d8801
xlfengye <2476083228@qq.com>
2026-06-22 22:30:06 +0800

    Update 494430.lua

--- 494430.lua
+++ 494430.lua
@@ -1,4 +1,2 @@
 addappid(494430)
-addappid(494431,0,"f3e90e6c386ee5c53b1d838df766777febca68f68ca94cbe4ae6f7f0087c5eed")
-setManifestid(494431,"2547679596161383708")
-addappid(494432)
+addappid(494431, 0, "f3e90e6c386ee5c53b1d838df766777febca68f68ca94cbe4ae6f7f0087c5eed")
