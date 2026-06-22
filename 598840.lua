a0b70486d147e698ee5524622a248e8f008eec88
2476083228 <1157079671@qq.com>
2026-06-22 16:02:10 +0800

    Update 598840.lua

--- 598840.lua
+++ 598840.lua
@@ -1,6 +1,3 @@
 addappid(598840)
-addappid(598841)
-addappid(598842)
-addappid(598843)
-addappid(598844,0,"ca105900ab565efe7237fcf8252698ce66d1c1a23b6fa5cdecc22fca9fedb0ac")
-setManifestid(598844,"6046470214164544330")
+addappid(598841, 0, "a35112cff08f989dc8b0eb0fcd6f1c64843fcac8f26991cf0792cd43ab01fcfc")
+addappid(598844, 0, "ca105900ab565efe7237fcf8252698ce66d1c1a23b6fa5cdecc22fca9fedb0ac")
