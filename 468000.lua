d0f6d5a42106f7dd515c7244ff8abc863d8820d1
2476083228 <1157079671@qq.com>
2026-06-22 20:24:40 +0800

    Update 468000.lua

--- 468000.lua
+++ 468000.lua
@@ -1,5 +1,2 @@
 addappid(468000)
-addappid(468001,0,"eda6c6d68b7598bc4fcb1434ff4f977aff1bc31e7dcddbad5bd3d11c5854de83")
-setManifestid(468001,"6384048671227885072")
-addappid(640810)
-addappid(468002)
+addappid(468001, 0, "eda6c6d68b7598bc4fcb1434ff4f977aff1bc31e7dcddbad5bd3d11c5854de83")
