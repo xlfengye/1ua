22b0b3d485dde55da3c8da9d4171ce3f77a39782
2476083228 <1157079671@qq.com>
2026-06-22 20:22:46 +0800

    Update 1255520.lua

--- 1255520.lua
+++ 1255520.lua
@@ -1,4 +1,2 @@
 addappid(1255520)
-addappid(1255521,0,"b11ba3deabf8c89ac32dccb2cad2e2f2e68baaf20ddd66bfea72ed1f7bf7b7a8")
-setManifestid(1255521,"3337309213557804447")
-addappid(1255522)
+addappid(1255521, 0, "b11ba3deabf8c89ac32dccb2cad2e2f2e68baaf20ddd66bfea72ed1f7bf7b7a8")
