f6e4266325dc43db779eb60a20ec55f5fc0cf9f2
2476083228 <1157079671@qq.com>
2026-06-23 00:30:51 +0800

    Update 1378840.lua

--- 1378840.lua
+++ 1378840.lua
@@ -1,6 +1,2 @@
 addappid(1378840)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
 addappid(1378841,0,"b8bfe363337be7f23bfb74e1133d3efb7cbf65b6ccd5726a2aaf9fcd3f46fd7c")
