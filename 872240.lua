cd9d644c16820db23c73039122d415f2d7a28e6a
2476083228 <1157079671@qq.com>
2026-06-22 16:03:45 +0800

    Update 872240.lua

--- 872240.lua
+++ 872240.lua
@@ -1,5 +1,2 @@
 addappid(872240)
-addappid(228982)
-setManifestid(228982,"6413394087650432851")
-addappid(872241,0,"7ad4aaffa223bda049dc72bd0f503b86b1443e7bc864eae45a8abae9a5fe14f2")
-addappid(872250)
+addappid(872241, 0, "7ad4aaffa223bda049dc72bd0f503b86b1443e7bc864eae45a8abae9a5fe14f2")
