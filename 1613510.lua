ddd19149827e16860d63c5d251acbb93d421236d
xlfengye <2476083228@qq.com>
2026-06-22 20:42:35 +0800

    Update 1613510.lua

--- 1613510.lua
+++ 1613510.lua
@@ -1,6 +1,2 @@
 addappid(1613510)
-addappid(228988)
-setManifestid(228988,"6645201662696499616")
-addappid(228990)
-setManifestid(228990,"1829726630299308803")
-addappid(1613511,0,"59dfc8cf7dcac3983580a19de7cd5d9819cecc6dfa2e3d55cefbb864a716d1fa")
+addappid(1613511, 0, "59dfc8cf7dcac3983580a19de7cd5d9819cecc6dfa2e3d55cefbb864a716d1fa")
