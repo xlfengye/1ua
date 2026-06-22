edab2ce15286bfac0160d1da9213c0d71ae263c3
2476083228 <1157079671@qq.com>
2026-06-22 16:02:50 +0800

    Update 699510.lua

--- 699510.lua
+++ 699510.lua
@@ -1,5 +1,2 @@
 addappid(699510)
-addappid(699511,0,"cc454c587bad73a6dfbfe8e7f9acc667edf4cfdc0151fa2f04f7851124d1ce54")
-setManifestid(699511,"4972160412586632649")
-addappid(699512)
-addappid(699513)
+addappid(699511, 0, "cc454c587bad73a6dfbfe8e7f9acc667edf4cfdc0151fa2f04f7851124d1ce54")
