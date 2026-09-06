285287aa8bf42c0f1fc6d1161f4f30d85afb34ee
xlfengye <2476083228@qq.com>
2026-09-06 16:24:22 +0800

    Update 3751260.lua

---
+++ 3751260.lua
@@ -0,0 +1,12 @@
+addappid(3751260, 1, "bfb8982d4aeb1b3b81978c18fb93b66cbc707cf9530ab92769d00c50e90c4031")
+
+addappid(3751261, 1, "fe44cc16911c3d5dabb2b862a70f1b8b58ee79e1da1f47b8bf03af487c17e8fc")
+setManifestid(3751261, "7317586772551391000", 58343195713)
+
+-- DLC's (no depot keys required)
+addappid(4417540) -- The Blood of Dawnwalker - Sangoran Wayfarer's Armor Set
+addappid(4417550) -- The Blood of Dawnwalker - Eclipse Edition Content
+
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+
