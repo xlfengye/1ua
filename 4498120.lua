6dc44fb0791bf0771e8b63e441ab53dd633a5e01
xlfengye <2476083228@qq.com>
2026-09-13 19:17:17 +0800

    Add files via upload

---
+++ 4498120.lua
@@ -0,0 +1,12 @@
+-- 4498120's Lua and Manifest Created by Hubcap Manifest
+-- Hentai Tales: Breed Me - Sensei
+-- Created: September 12, 2026 at 15:00:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4498120) -- Hentai Tales: Breed Me - Sensei
+-- MAIN APP DEPOTS
+addappid(4498121, 1, "7b0a087d49a9d56055aad607ef98a9fa6dd68eec095b93dc263071de877b9bf6") -- Depot 4498121
+setManifestid(4498121, "8080001083391805761", 634761246)
